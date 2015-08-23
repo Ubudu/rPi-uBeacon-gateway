de0fdda59e1c238e4ef7cbf74ce83ba2565f86d1
Matthieu Balmes <matthieu.balmes@gmail.com>
2015-08-23 12:35:04 +0200

    Create page to display network infos

--- routes/home.js
+++ routes/home.js
@@ -1,13 +1,11 @@
+'use strict';
+
 var express = require('express');
-var router = new express.Router();
+var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
-});
-
-router.get('/configure-my-beacon', function(req, res) {
-    res.render('index', { title: 'Express' });
+  res.render('home/index', { title: 'Express' });
 });
 
 module.exports = { path: '/', router: router };
---
+++ routes/network.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var express = require('express');
+var ip = require('ip');
+var os = require('os');
+var router = express.Router();
+
+/* Set the correct header section */
+router.use(function(req, res, next) {
+    res.locals.headerSection = 'network_infos';
+    next();
+});
+
+/* GET /network-infos page */
+router.get('/', function(req, res, next) {
+    var networkInterfaces = os.networkInterfaces();
+    console.log(networkInterfaces);
+
+    // Select the relevant network interface
+    var networkInterfaceName = process.env.NETWORK_INTERFACE_NAME;
+
+    if (networkInterfaces.hasOwnProperty(networkInterfaceName)) {
+        var networkInterfaceTypes = networkInterfaces[networkInterfaceName];
+        return res.render('network/index', { networkInterfaceTypes: networkInterfaceTypes });
+    } else {
+        var err = new Error('Network interface not found. The RaspberryPi is probably misconfigured');
+        err.status = 500;
+        return next(err);
+    }
+});
+
+module.exports = { path: '/network-infos', router: router };
