2b5c64fd0a01818e273be976b205629ac2d9c3fa
Matthieu Balmes <matthieu.balmes@gmail.com>
2015-08-31 10:47:41 +0200

    Merge system and network page + update header

--- routes/system.js
+++ routes/system.js
@@ -6,6 +6,8 @@ var moment = require('moment');
 require('moment-duration-format');
 var router = new express.Router();
 
+var MISCONFIGURATION_MSG = 'Network interface not found. The RaspberryPi is probably misconfigured'; // eslint-disable-line
+
 /* Set the correct header section */
 router.use(function(req, res, next) {
     res.locals.headerSection = 'system_infos';
@@ -17,13 +19,27 @@ router.get('/', function(req, res) {
     var uptime = moment.duration(os.uptime(), 'seconds');
     var uptimeDuration = uptime.format('d [days] hh:mm:ss');
 
+    var interfaces = os.networkInterfaces();
+
+    // Select the relevant network interface
+    var interfaceName = process.env.NETWORK_INTERFACE_NAME;
+
+    if (interfaces.hasOwnProperty(interfaceName)) {
+        var interfaceTypes = interfaces[interfaceName];
+    } else {
+        var err = new Error(MISCONFIGURATION_MSG);
+        err.status = 500;
+        return next(err);
+    }
+
     res.render('system/index', {
         platform: os.platform(),
         uptime: uptimeDuration,
         loadavg: os.loadavg(),
         hostname: os.hostname(),
         freemem: Math.round(os.freemem() / (1024 * 1024)),
-        totalmem: Math.round(os.totalmem() / (1024 * 1024))
+        totalmem: Math.round(os.totalmem() / (1024 * 1024)),
+        interfaceTypes: interfaceTypes
     });
 });
 
