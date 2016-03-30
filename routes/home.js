9e4a48a7f7c390ccd50d0f5c27caa09b0a714644
Matthieu Balmes <matthieu.balmes@ubudu.com>
2016-03-30 21:02:59 +0200

    Use username for messages

--- routes/home.js
+++ routes/home.js
@@ -2,10 +2,25 @@
 
 var express = require('express');
 var router = new express.Router();
+var observer = require('node-observer');
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('home/index', { title: 'Express' });
+});
+
+/* GET welcome VIP */
+router.get('/welcome', function(req, res, next) {
+  var username = req.query.user_name;
+
+  console.log(req.query);
+
+  observer.send(this, 'messages:message', { message: 'Automatic notification from user: ' + username });
+
+  return res.json({
+    "decision": "notify",
+    "notify_user": "{\"alertBody\":\"Un vendeur vient vers vous.\" }"
+  });
 });
 
 module.exports = { path: '/', router: router };
